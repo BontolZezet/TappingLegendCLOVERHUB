841d18061a1d914d589f3823d1811119406becd8
BontolZezet <106383791+BontolZezet@users.noreply.github.com>
2022-06-27 08:35:16 +0700

    Add files via upload

---
+++ tappinglegend.lua
@@ -0,0 +1,138 @@
+--Tapping legends script
+getgenv().autotap = false;
+getgenv().autorebirth = false;
+getgenv().autoupgradetapmultiplier = false;
+getgenv().autoupgradeautoclicker = false;
+getgenv().autoupgrademorerebirth = false;
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("Cloveor Hub", "Ocean")
+
+local w = Window:NewTab("Farming")
+
+
+local w = w:NewSection("Auto Upgrade")
+local w = a:NewSection("Auto Farm")
+
+
+
+
+a:NewToggle("Clicker Multiplier", "Upgrade Click Multiplier", function(state)
+    if state then
+	getgenv().autoupgradetapmultiplier = true;
+	ClikerMultiplier()
+    else
+    getgenv().autoupgradetapmultiplier = false;
+    end
+end)
+
+a:NewToggle("More Rebirth", "Upgrade Rebirth value", function(state)
+    if state then
+	getgenv().autoupgrademorerebirth = true;
+	MoreRebirth()
+    else
+    getgenv().autoupgrademorerebirth = false;
+    end
+end)
+
+a:NewToggle("Auto Clicker", "Upgrade Auto Clicker", function(state)
+    if state then
+	getgenv().autoupgradeautoclicker = true;
+	AutoClicker()
+    else
+    getgenv().autoupgradeautoclicker = false;
+    end
+end)
+
+
+w:NewToggle("Auto Tap", "auto clicker", function(state)
+    if state then
+	getgenv().autotap = true;
+	doatap()
+    else
+    getgenv().autotap = false;
+    end
+end)
+
+
+
+
+function doatap()
+	spawn(function()
+		while getgenv().autotap == true do
+			game:GetService("ReplicatedStorage").Remotes.Tap:FireServer()
+			wait()
+		end
+	end)
+end
+
+doatap();
+
+
+function autorebirtoh()
+	spawn(function()
+		while getgenv(),autorebirth == true do
+			local args = {
+				[1] = 1
+			}
+			
+			game:GetService("ReplicatedStorage").Remotes.Rebirth:FireServer(unpack(args))
+			wait()
+		end
+	end)
+end
+
+autorebirtoh();
+
+
+
+function ClikerMultiplier()
+	spawn(function()
+		while autoupgradetapmultiplier == true do
+			local args = {
+				[1] = "Ruby",
+				[2] = "TapMultiplier"
+			}
+			
+			game:GetService("ReplicatedStorage").Remotes.Upgrade:InvokeServer(unpack(args))
+			wait()
+		end
+	end)
+end
+
+ClikerMultiplier();
+
+
+function AutoClicker()
+	spawn(function()
+		while autoupgradeautoclicker == true do 
+			local args = {
+				[1] = "Ruby",
+				[2] = "FreeAutoClicker"
+			}
+			
+			game:GetService("ReplicatedStorage").Remotes.Upgrade:InvokeServer(unpack(args))
+			wait()
+		end
+	end)
+end
+
+AutoClicker();
+
+
+function MoreRebirth()
+	spawn(function()
+		while autoupgrademorerebirth == true do 
+			local args = {
+				[1] = "Ruby",
+				[2] = "RebirthsUpgrade"
+			}
+			
+			game:GetService("ReplicatedStorage").Remotes.Upgrade:InvokeServer(unpack(args))
+			wait()
+		end	
+	end)
+end
+
+MoreRebirth();
